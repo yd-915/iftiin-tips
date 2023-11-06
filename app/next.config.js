940fed3037bedf7949983b4e390e57f7b8e2c954
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-06 16:29:17 -0500

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-restricted-imports, @typescript-eslint/no-var-requires
 const { i18n } = require("./next-i18next.config");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withSentryConfig } = require("@sentry/nextjs");
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -23,21 +23,7 @@ const nextConfig = {
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-  disableServerWebpackPlugin: false,
-  disableClientWebpackPlugin: false,
-  org: "lightsats",
-  project: "lightsats",
-};
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig(nextConfig);
