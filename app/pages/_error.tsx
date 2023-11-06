e5d121571a5ffbdd89c83f109a56c84649f3b1ea
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-06 16:32:05 -0500

    Update _error.tsx

--- app/pages/_error.tsx
+++ app/pages/_error.tsx
@@ -16,7 +16,6 @@
  *  - https://reactjs.org/docs/error-boundaries.html
  */
 
-import * as Sentry from "@sentry/nextjs";
 import { NextPageContext } from "next";
 import NextErrorComponent from "next/error";
 
@@ -31,7 +30,6 @@ const CustomErrorComponent = (props: { statusCode: number }) => {
 CustomErrorComponent.getInitialProps = async (contextData: NextPageContext) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
 
   // This will contain the status code of the response
   return NextErrorComponent.getInitialProps(contextData);
